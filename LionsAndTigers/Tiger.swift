4f1e8a57fbc4f73aa1b17adc046ab5105ec53698
bitfountain-student <bitfountain@slowchat.us>
2015-06-29 16:55:44 -0700

    The git repository was damaged and had to be deleted.
    Here is the history:
    
    commit 357937eac005bd3aa182767f4d1dd59e5229ba30
    Author: bitfountain-student <bitfountain@slowchat.us>
    Date:   Mon Jun 29 00:06:53 2015 -0700
    
        completed: Multiple Tigers
    
    commit aff5c73acb117fb30ca94b420dd019f5bffc26db
    Author: bitfountain-student <bitfountain@slowchat.us>
    Date:   Sun Jun 28 18:16:14 2015 -0700
    
        completed: Adding Properties to the Tiger
    
    commit 3cac08927db06ce2648fbd8935c7f46b9963dc6f
    Author: bitfountain-student <bitfountain@slowchat.us>
    Date:   Sun Jun 28 17:58:08 2015 -0700
    
        completed: Make Your Own Struct
    
    commit 5a4131dbafd97c72c580fd64eaf24a2091dcb861
    Author: bitfountain-student <bitfountain@slowchat.us>
    Date:   Sun Jun 28 17:52:47 2015 -0700
    
        completed: Create a New Project and Storyboard Setup

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,17 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by gambord on 6/28/15.
+//  Copyright (c) 2015 bitfountain-student. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+  var age = 0
+  var name = ""
+  var breed = ""
+  var image = UIImage(named:"")
+}
